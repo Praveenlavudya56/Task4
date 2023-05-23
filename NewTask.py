1248d420bbcea9ddcb9da4a30f9f34153ff6f2ad
unknown <praveenlavudya56@gmail.com>
2023-05-23 18:29:13 +0530

    i23-05-23 Task

---
+++ NewTask.py
@@ -0,0 +1,14 @@
+#input ="praveen is good boy"
+#output="boy good is praveen"
+
+# a="praveen is good boy"
+# split=a.split()
+# b=split[::-1]
+# join=" ".join(b)  
+# print(join)
+
+# a="ABBCDEFFGGHIJKL"
+# b=list(set(a))
+# b.sort()
+# output=''.join(b)
+# print(output)
